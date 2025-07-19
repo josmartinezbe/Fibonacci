c7d2ecf614a7ec03a4dff8a6f03e5b9de5f4829a
josmartinezbe <sebastianmartinez66@gmail.com>
2025-07-18 20:16:42 -0500

    ejercicio fibonacci

---
+++ factorialeje.java
@@ -0,0 +1,11 @@
+public class SerieFibo{
+	public static int fibo(int a){
+		if (a > 2){
+		    return fibo(a-1)+fibo(a-2);
+		else
+		return 1;
+	}
+	public static void main(String [] args){
+		System.out.println(fibo(6));
+	}
+} 
